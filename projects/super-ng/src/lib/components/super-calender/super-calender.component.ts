6056a008a8eac439c59acdee491a351fe5acdc25
raviv123 <ravivermawins@gmail.com>
2025-02-18 18:34:49 +0530

    Implemented new changes

--- projects/super-ng/src/lib/components/super-calender/super-calender.component.ts
+++ projects/super-ng/src/lib/components/super-calender/super-calender.component.ts
@@ -1,48 +1,63 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, inject, Input, model, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ComponentRef, EventEmitter, inject, Input, model, OnInit, Output, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 import { MatCalendar, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarOptions, EventApi } from '@fullcalendar/core';
+import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
 import timeGridPlugin from '@fullcalendar/timeGrid';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { SuperDialogComponent } from '../../../public-api';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { SubscriptionUtils } from '../../utils/subscription.utils';
 
 
 
 @Component({
   selector: 's-calender',
   standalone: true,
-  imports: [FullCalendarModule, MatCardModule, MatDatepickerModule, MatMenuModule,MatFormFieldModule, MatSelectModule, MatInputModule, MatNativeDateModule, CommonModule, FormsModule, MatTableModule, MatButtonModule, MatIconModule, MatDialogModule, SuperDialogComponent],
+  imports: [FullCalendarModule, MatCardModule, MatDatepickerModule, MatMenuModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatNativeDateModule, CommonModule, FormsModule, MatTableModule, MatButtonModule, MatIconModule, MatDialogModule, SuperDialogComponent],
   templateUrl: './super-calender.component.html',
   styleUrl: './super-calender.component.scss',
   providers: [provideNativeDateAdapter()],
   encapsulation: ViewEncapsulation.None,
 
 })
-export class SuperCalenderComponent implements OnInit, AfterViewInit {
+export class SuperCalenderComponent extends SubscriptionUtils implements OnInit, AfterViewInit {
+
+  @Output() dateTimeClick = new EventEmitter();
+  @Output() initializedSuccess = new EventEmitter();
+  @Output() afterDialogOpened = new EventEmitter();
+
+
+  @Input() readonlyView: boolean = false;
+  @Input() availableViews: string[] = ['Month', 'Week', 'Day'];
+  @Input() showLeftSidebar: boolean = true;
+
+  @Input() createButtonText: string = 'Create';
+
   @ViewChild('calendar') calendarComponent!: FullCalendarComponent;
-  @ViewChild('datepicker') datepicker!: MatCalendar<any>;
+  @ViewChild('datepicker') datepicker!: MatCalendar<Date>;
 
   @Input() events: any[] = [];
 
-  calenderViews: any[] = [
-    { value: 'dayGridMonth', viewValue: 'Month' },
-    { value: 'timeGridWeek', viewValue: 'Week' },
-    { value: 'timeGridDay', viewValue: 'Day' },
+  allCalenderViews: DropdownOption[] = [
+    { value: 'dayGridMonth', label: 'Month' },
+    { value: 'timeGridWeek', label: 'Week' },
+    { value: 'timeGridDay', label: 'Day' },
   ];
+
+  calenderViews: DropdownOption[] = [];
 
   selectedView: string = '';
 
@@ -52,29 +67,35 @@ export class SuperCalenderComponent implements OnInit, AfterViewInit {
   isDayView: boolean = false
 
   ngOnInit(): void {
-    this.intialize()
+    this.calenderViews = this._getCalendarViews();
+    this.selectedView = this.calenderViews[0].value
+    this.calendarOptions = this._getCalendarOptions();
   }
 
   ngAfterViewInit(): void {
-    this.datepicker._userSelection.subscribe((event) => {
-      const selectedDate = this.datepicker.selected as Date;
-      const calendarApi = this.calendarComponent.getApi();
-      if (selectedDate && this.compareDatesOnly(selectedDate, event.value) === 0) {
-        calendarApi.changeView(calendarApi.view.type == 'timeGridDay' ? 'dayGridMonth' : 'timeGridDay');
-      }
-    });
+    this._addSubscriptions();
+    this.initializedSuccess.emit(this);
   }
 
-  compareDatesOnly(d1: Date, d2: Date): number {
-    const date1 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate());
-    const date2 = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate());
-
-    return date1.getTime() - date2.getTime();
+  private _getCalendarViews(): DropdownOption[] {
+    const currentViews = this.availableViews.map(view => view.toLowerCase());
+    return this.allCalenderViews.filter(option => currentViews.includes(option.label.toLowerCase()));
   }
 
-  intialize() {
-    this.selectedView = this.calenderViews[0].value
-    this.calendarOptions = {
+  private _addSubscriptions() {
+    this.subscription.add(
+      this.datepicker._userSelection.subscribe((event) => {
+        const selectedDate = this.datepicker.selected as Date;
+        const calendarApi = this.calendarComponent.getApi();
+        if (selectedDate && this.areSameDates(selectedDate, event.value as Date)) {
+          calendarApi.changeView(calendarApi.view.type == 'timeGridDay' ? 'dayGridMonth' : 'timeGridDay');
+        }
+      })
+    );
+  }
+
+  private _getCalendarOptions(): CalendarOptions {
+    return {
       plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
       nowIndicator: true,
       initialView: 'dayGridMonth', // Default to month view
@@ -103,7 +124,7 @@ export class SuperCalenderComponent implements OnInit, AfterViewInit {
       editable: true, // Enables drag & drop
       selectable: true, // Enables selection
       dateClick: this.handleDateClick.bind(this), // Click event
-      select: this.handleSelect.bind(this),  // Range selection
+      // select: this.handleSelect.bind(this),  // Range selection
       eventClick: this.handleEventClick.bind(this),
       // eventDrop: this.handleEventDrop.bind(this),
       events: this.events
@@ -112,6 +133,15 @@ export class SuperCalenderComponent implements OnInit, AfterViewInit {
       // ]
     };
   }
+
+  areSameDates(d1: Date, d2: Date): boolean {
+    const date1 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate());
+    const date2 = new Date(d2.getFullYear(), d2.getMonth(), d2.getDate());
+
+    return (date1.getTime() - date2.getTime()) === 0;
+  }
+
+
 
   onViewChange(event: MatSelectChange) {
     const calendarApi = this.calendarComponent.getApi();
@@ -125,41 +155,42 @@ export class SuperCalenderComponent implements OnInit, AfterViewInit {
   onDateSelect(event: any) {
     const calendarApi = this.calendarComponent.getApi();
     calendarApi.gotoDate(event);
-
   }
 
   onCreateClick() {
-    // this.openDialog()
+    const calendarApi = this.calendarComponent.getApi();
+    this.openDialog(calendarApi, 'createEventGLobal')
   }
 
   handleDateClick(event: any) {
-    debugger
-    this.openDialog(event)
+    this.dateTimeClick.emit(event);
+    if (this.readonlyView) return;
+
+    this.openDialog(event, 'onCalenderClick');
   }
 
-  handleSelect(arg: any) {
-
-  }
-
-
-  triggerDayView() {
-
-  }
-
-  openDialog(event: any) {
+  openDialog(event: any, action: string) {
     const dialogRef = this.dialog.open(SuperDialogComponent, {
       data: {
         event: event,
+        action: action
       },
       // width: '250px',
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      const calendarApi = this.calendarComponent.getApi();
-      calendarApi.addEvent(result.data)
-    });
+    this.afterDialogOpened.emit(dialogRef);
+
+    this.subscription.add(
+      dialogRef.afterClosed().subscribe(result => {
+        const calendarApi = this.calendarComponent.getApi();
+        calendarApi.addEvent(result.data)
+      })
+    );
   }
 
 }
 
-
+interface DropdownOption {
+  label: string,
+  value: string
+}
--- projects/super-ng/src/lib/components/super-dialog/s-dialog.component.ts
+++ projects/super-ng/src/lib/components/super-dialog/s-dialog.component.ts
@@ -4,102 +4,176 @@ import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 // import { MatTypographyModule } from '@angular/material/core';
 import { NgxMatTimepickerComponent, NgxMatTimepickerDirective, NgxMatTimepickerModule } from 'ngx-mat-timepicker';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatMenuModule } from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
-import {MatIconModule} from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 's-dialog',
   standalone: true,
-  imports: [MatDialogContent,MatIconModule, MatDialogActions, MatDialogClose, MatButtonModule, MatFormFieldModule, FormsModule, MatInputModule, MatDatepickerModule,NgxMatTimepickerModule, NgxMatTimepickerComponent,NgxMatTimepickerDirective,NgxMatTimepickerModule,MatCheckboxModule, MatMenuModule,MatFormFieldModule, MatSelectModule, MatInputModule,CommonModule ],
+  imports: [MatDialogContent, MatIconModule, MatDialogActions, MatDialogClose, MatButtonModule, MatFormFieldModule, FormsModule, MatInputModule, MatDatepickerModule, NgxMatTimepickerModule, NgxMatTimepickerComponent, NgxMatTimepickerDirective, NgxMatTimepickerModule, MatCheckboxModule, MatMenuModule, MatFormFieldModule, MatSelectModule, MatInputModule, CommonModule],
   templateUrl: './s-dialog.component.html',
   styleUrl: './s-dialog.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
 export class SuperDialogComponent {
 
-  mytime: any
-  timeFrom: string = '';
-  timeTo: string = '';
-  dateFrom: string = '';
-  dateTo: string = '';
-  date: string = '';
-
-  allDaySelected: boolean = false;
-  allDayChecked: boolean = false;
-
-  foods: any[] = [
-    {value: 'steak-0', viewValue: '10:00AM'},
-    {value: 'pizza-1', viewValue: '10:00AM'},
-    {value: 'tacos-2', viewValue: '10:00AM'},
-  ];
-
-  constructor( public dialogRef: MatDialogRef<SuperDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
-      this.Initialize()
-    }
-
-    Initialize() {
-      debugger
-      const date = new Date(this.data.event.dateStr);
-      if(this.data.event.view.type == 'dayGridMonth') {
-        this.allDaySelected = true;
-        // allDayChecked
-        
-      }
-      const timeFrom = date.toLocaleTimeString("en-US", { hour12: false });
-      date.setHours(date.getHours() + 1);
-      const timeTo = date.toLocaleTimeString("en-US", { hour12: false });
-      
-      if(this.allDaySelected) {
-        this.dateFrom = this.data.event.dateStr;
-        this.dateTo = this.data.event.dateStr;
-        this.date = this.data.event.dateStr;
-        this.timeFrom = "10:00:00";
-        this.timeTo = "11:00:00";
-      } else {
-        this.dateFrom = this.data.event.dateStr;
-        this.dateTo = this.data.event.dateStr;
-        this.date = this.data.event.dateStr;
-        this.timeFrom = timeFrom;
-        this.timeTo = timeTo;
-      }
-    }
-
-  closeDialog() {
-    this.dialogRef.close({data: { title: 'Meeting', start: '2025-02-15T10:00:00' }});
+  controls = {
+    timeFrom: '',
+    timeTo: '',
+    dateFrom: '',
+    dateTo: '',
+    date: '',
+    eventReps: '',
+    allDaySelected: false
   }
 
-  // timeSlots: string[] = this.generateTimeSlots();
+  // allDaySelected: boolean = false;
+  allDayChecked: boolean = false;
 
-  // generateTimeSlots(): string[] {
-  //   const times: string[] = [];
-  //   for (let hour = 0; hour < 24; hour++) {
-  //     for (let minutes = 0; minutes < 60; minutes += 30) {
-  //       const h = hour.toString().padStart(2, '0');
-  //       const m = minutes.toString().padStart(2, '0');
-  //       times.push(`${h}:${m}`);
-  //     }
-  //   }
-  //   return times;
-  // }
+  repeatEventsOptions: any[] = [
+    { value: 'notRepeat', label: 'Does not repeat' },
+    { value: 'daily', label: 'Daily' }
+  ];
+
+  constructor(public dialogRef: MatDialogRef<SuperDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.Initialize()
+  }
+
+  Initialize() {
+    if (this.data.action == 'onCalenderClick') {
+      if (this.data.event.view.type == 'dayGridMonth') {
+        this.controls.allDaySelected = true;
+      }
+
+      this.controls.eventReps = 'notRepeat';
+
+      const timeFrom = this.formatTime(this.data.event.dateStr);
+      const timeTo = this.formatTime(this.data.event.dateStr, true);
+
+
+      if (this.controls.allDaySelected) {
+        this.controls.dateFrom = this.data.event.dateStr;
+        this.controls.dateTo = this.data.event.dateStr;
+        this.controls.date = this.data.event.dateStr;
+        this.controls.timeFrom = "10:00 AM";
+        this.controls.timeTo = "11:00 AM";
+      } else {
+        this.controls.dateFrom = this.data.event.dateStr;
+        this.controls.dateTo = this.data.event.dateStr;
+        this.controls.date = this.data.event.dateStr;
+        this.controls.timeFrom = timeFrom;
+        this.controls.timeTo = timeTo;
+      }
+    }
+    else if (this.data.action == 'createEventGLobal') {
+      // if (this.data.event.view.type == 'dayGridMonth') {
+      this.controls.allDaySelected = false;
+      this.controls.eventReps = 'notRepeat';
+      // allDayChecked
+      // }
+
+      const currentDate = this.data.event.getDate();
+      const formattedDate = currentDate.toISOString().split('T')[0];
+
+      if (this.controls.allDaySelected) {
+        this.controls.dateFrom = formattedDate;
+        this.controls.dateTo = formattedDate;
+        this.controls.date = formattedDate;
+        this.controls.timeFrom = "08:00 AM";
+        this.controls.timeTo = "09:00 AM";
+      } else {
+        this.controls.dateFrom = formattedDate;
+        this.controls.dateTo = formattedDate;
+        this.controls.date = formattedDate;
+        this.controls.timeFrom = "08:00 AM";
+        this.controls.timeTo = "09:00 AM";
+      }
+
+    }
+  }
+
+  closeDialog() {
+    this.dialogRef.close({ data: { title: 'Meeting', start: '2025-02-15T10:00:00' }, controls: this.controls });
+  }
+
+  timeSlots: any[] = this.generateTimeSlots();
+
+  generateTimeSlots(): { value: string; viewValue: string }[] {
+    const times: { value: string; viewValue: string }[] = [];
+
+    for (let hour = 0; hour < 24; hour++) {
+      for (let minutes = 0; minutes < 60; minutes += 15) {
+        const period = hour < 12 ? 'AM' : 'PM';
+        let displayHour = hour % 12 || 12; // Convert 0 to 12 for AM/PM format
+        const h = displayHour.toString().padStart(2, '0');
+        const m = minutes.toString().padStart(2, '0');
+
+        const time12HourFormat = `${h}:${m} ${period}`;
+        times.push({ value: time12HourFormat, viewValue: time12HourFormat });
+      }
+    }
+
+    return times;
+  }
 
   // selectTime(time: string) {
   //   this.TimeFrom = time;
   // }
 
+  onTimeSlotFromChange(event: any) {
+    this.controls.timeFrom = event.value;
+  }
+
+  onTimeSlotToChange(event: any) {
+    this.controls.timeTo = event.value;
+
+  }
+
+
+  onFromDateChange(event: any) {
+    this.controls.dateFrom = event.value
+  }
+
+  onToDateChange(event: any) {
+    this.controls.dateTo = event.value
+  }
+
   onCheckboxChange(event: any) {
-    debugger
-    this.dateFrom = this.data.event.dateStr;
-    if(event.checked) {
-      this.allDaySelected = true;
+    // this.dateFrom = this.data.event.dateStr;
+    if (event.checked) {
+      this.controls.allDaySelected = true;
     } else {
-      this.allDaySelected = false;
+      this.controls.allDaySelected = false;
     }
+  }
+
+  formatTime(dateTime: string, plusOneHours: boolean = false): string {
+    const date = new Date(dateTime);
+
+    if (plusOneHours)
+      date.setHours(date.getHours() + 1);
+
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const period = hours < 12 ? 'AM' : 'PM';
+
+    // Convert 24-hour format to 12-hour format
+    hours = hours % 12 || 12;
+
+    // Format hours and minutes properly
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${period}`;
+
+    return formattedTime;
+  }
+
+  selectEventReps(event: any) {
+    this.controls.eventReps = event.value;
   }
 }
